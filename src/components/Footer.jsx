5c3d21844ccd136decef98bb6e52d74e12d9d163
akhilrb <148519467+akhilrb@users.noreply.github.com>
2025-02-06 18:15:03 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -49,3 +49,5 @@ const Footer = () => {
 };
 
 export default Footer; 
+<script async type='module' src='https://interfaces.zapier.com/assets/web-components/zapier-interfaces/zapier-interfaces.esm.js'></script>
+<zapier-interfaces-chatbot-embed is-popup='false' chatbot-id='cm6htgghh0016lhc21svhtgs7' height='600px' width='400px'></zapier-interfaces-chatbot-embed>
